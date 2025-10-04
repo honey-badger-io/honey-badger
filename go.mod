4b577dab84fd84d320742c9feeaa390738a04bdf
Miron Jakubowski <mijakubowski@gmail.com>
2025-10-04 12:35:17 +0200

    Test server with redis Go client

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/dgraph-io/badger/v4 v4.8.0
+	github.com/redis/go-redis/v9 v9.14.0
 	github.com/stretchr/testify v1.10.0
 )
 
@@ -11,6 +12,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
--- server/server_test.go
+++ server/server_test.go
@@ -1,154 +1,89 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/honey-badger-io/honey-badger/config"
 	"github.com/honey-badger-io/honey-badger/db"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
 )
 
-const (
-	DbName = "db0"
-)
-
-func TestDbServer(t *testing.T) {
-	server := startServer()
+func TestServer(t *testing.T) {
+	var ctx = context.Background()
+	server, client := startServer(ctx)
 	defer server.Stop()
+	defer client.Close()
 
-	/*
-		client := pb.NewDbClient(conn)
-		const db = "test-create-db"
+	t.Run("should call set", func(t *testing.T) {
+		const key = "key"
+		const value = "value"
 
-		t.Run("should call create database", func(t *testing.T) {
-			_, err := client.Create(context.TODO(), &pb.CreateDbReq{
-				Name: db,
-				Opt: &pb.CreateDbOpt{
-					InMemory: true,
-				},
-			})
+		err := client.Set(ctx, key, value, 0).Err()
 
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+	})
 
-		t.Run("should call drop database", func(t *testing.T) {
-			_, err := client.Drop(context.TODO(), &pb.DropDbRequest{
-				Name: db,
-			})
+	t.Run("should call get", func(t *testing.T) {
+		const key = "key"
+		const value = "value"
 
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
+		val, err := client.Get(ctx, key).Result()
 
-		t.Run("should call exists database", func(t *testing.T) {
-			res, err := client.Exists(context.TODO(), &pb.ExistsDbReq{
-				Name: "test-db-exists",
-			})
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+		assert.Equal(t, value, val)
+	})
 
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-			assert.False(t, res.Exists)
-		})
+	t.Run("should call delete", func(t *testing.T) {
+		const key = "key"
 
-		t.Run("should call ensure db", func(t *testing.T) {
-			_, err := client.EnsureDb(context.TODO(), &pb.CreateDbReq{
-				Name: "test-db-ensure",
-				Opt: &pb.CreateDbOpt{
-					InMemory: true,
-				},
-			})
+		err := client.Del(ctx, key).Err()
 
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
-	*/
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+	})
+
+	t.Run("should call select", func(t *testing.T) {
+		err := client.Conn().Select(ctx, 1).Err()
+
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+	})
+
+	t.Run("should call ping", func(t *testing.T) {
+		val, err := client.Ping(ctx).Result()
+
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+		assert.Equal(t, "PONG", val)
+	})
+
+	t.Run("should call hello with empty name", func(t *testing.T) {
+		val, err := client.Conn().Hello(ctx, 3, "", "", "").Result()
+
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+		assert.Equal(t, "honey-badger", val["server"])
+		assert.Equal(t, "0.0.0", val["version"])
+		assert.Equal(t, int64(3), val["proto"].(int64))
+		assert.Equal(t, "standalone", val["mode"])
+		assert.Equal(t, "master", val["role"])
+		assert.Greater(t, val["id"].(int64), int64(0))
+	})
+
+	t.Run("should call hello with name", func(t *testing.T) {
+		const connectionName = "test"
+		err := client.Conn().Hello(ctx, 3, "", "", connectionName).Err()
+
+		assert.Nil(t, err, fmt.Sprintf("%v", err))
+	})
 }
 
-func TestDataServer(t *testing.T) {
-	server := startServer()
-	defer server.Stop()
-
-	/*
-		client := pb.NewDataClient(conn)
-		db := pb.NewDbClient(conn)
-
-		_, err := db.Create(context.TODO(), &pb.CreateDbReq{
-			Name: DbName,
-			Opt: &pb.CreateDbOpt{
-				InMemory: true,
-			},
-		})
-		if err != nil {
-			panic(err)
-		}
-
-		t.Run("should call set", func(t *testing.T) {
-			_, err := client.Set(context.TODO(), &pb.SetRequest{
-				Db:   DbName,
-				Key:  "test-key",
-				Data: []byte("test"),
-			})
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
-
-		t.Run("should call get", func(t *testing.T) {
-			_, err := client.Get(context.TODO(), &pb.KeyRequest{
-				Db:  DbName,
-				Key: "test-key",
-			})
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
-
-		t.Run("should call delete", func(t *testing.T) {
-			_, err := client.Delete(context.TODO(), &pb.KeyRequest{
-				Db:  DbName,
-				Key: "test-test",
-			})
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
-
-		t.Run("should call delete by prefix", func(t *testing.T) {
-			_, err := client.DeleteByPrefix(context.TODO(), &pb.PrefixRequest{
-				Db:     DbName,
-				Prefix: "test-",
-			})
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-		})
-
-		t.Run("should call create send stream", func(t *testing.T) {
-			stream, err := client.CreateSendStream(context.TODO())
-			if err != nil {
-				panic(err)
-			}
-
-			sendErr := stream.Send(&pb.SendStreamReq{
-				Db: DbName,
-			})
-
-			_, err = stream.CloseAndRecv()
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-			assert.Nil(t, sendErr, fmt.Sprintf("%v", err))
-		})
-
-		t.Run("should call get data stream", func(t *testing.T) {
-			prefix := "data-stream-"
-			res, err := client.CreateReadStream(context.TODO(), &pb.ReadStreamReq{
-				Db:     DbName,
-				Prefix: &prefix,
-			})
-
-			_, errRecv := res.Recv()
-
-			assert.Nil(t, err, fmt.Sprintf("%v", err))
-			assert.Equal(t, io.EOF, errRecv)
-		})
-	*/
-}
-
-func startServer() *Server {
+func startServer(ctx context.Context) (*Server, *redis.Client) {
 	port := 18950
-	//target := fmt.Sprintf("127.0.0.1:%d", port)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("localhost:%d", port),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
 	dbCtx := db.CreateCtx(config.BadgerConfig{
 		DataDirPath: "data",
@@ -156,9 +91,16 @@ func startServer() *Server {
 	})
 	server := New(config.ServerConfig{
 		Port: uint16(port),
-	}, dbCtx)
+	}, dbCtx, "0.0.0")
 
 	go server.Start()
 
-	return server
+	for {
+		err := rdb.Ping(ctx).Err()
+		if err == nil {
+			break
+		}
+	}
+
+	return server, rdb
 }
